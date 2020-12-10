19f9454d802ab3ff3443a43d6bd166b5bc96b13c
Lvthan <75730925+lvthan@users.noreply.github.com>
2020-12-10 12:34:49 +0800

    Add files via upload

---
+++ monster_vault/config.lua
@@ -0,0 +1,38 @@
+Config                        = {}
+Config.Locale                 = 'en'
+Config.VaultBox = '' 
+Config.Vault = {
+	vault = {
+		coords = vector3(-3226.04, 811.37, 8.93),
+		heading = 6.55,
+		needItemLicense = 'key1', --'licence_vault' -- If you don't want to use items Allow you to leave it blank or needItemLicense = nil
+		InfiniteLicense = true -- Should one License last forever?
+	},
+	vault = {
+		coords = vector3(-18.32, -1439.14, 31.1),
+		heading = 6.55,
+		needItemLicense = 'ktp', --'licence_vault' -- If you don't want to use items Allow you to leave it blank or needItemLicense = nil
+		InfiniteLicense = false -- Should one License last forever?
+	},
+	ambulance = {
+		coords = vector3(310.38, -603.09, 43.28),
+		heading = 71.88,
+	},
+	pedagang = {
+		coords = vector3(-634.45, 228.07, 81.88),
+		heading = 298.7,
+	},
+	mafia = {
+		coords = vector3(-1531.74, 142.74, 55.67),
+		heading = 298.7,
+	},
+	mechanic = {
+		coords = vector3(-206.64, -1341.4, 34.89),
+		heading = 298.7,
+	},
+	police = {
+		coords = vector3(458.64, -979.08, 30.69),
+		heading = 71.88,
+	},
+}
+
---
+++ monster_vault/vault.sql
@@ -0,0 +1,16 @@
+USE `essentialmode`;
+
+
+
+INSERT INTO `addon_account` (name, label, shared) VALUES
+  ('vault_black_money', 'Money Vault', 0),
+  ('society_police_black_money', 'Police Black Money', 1) ------------------[untuk black money coba contoh yang poilisi biar bekerja ke all job ]
+;
+
+INSERT INTO `addon_inventory` (name, label, shared) VALUES
+  ('vault', 'Vault', 0) 
+;
+
+INSERT INTO `datastore` (name, label, shared) VALUES
+  ('vault', 'Vault', 0)
+;
